709f0728ee66734a66f9b966f0d8281555e38a16
Bram De Coninck <bram@codifly.be>
2019-05-28 13:49:41 +0200

    Prepare application for second experiment

--- CocktailrXamarin/CocktailrXamarin.Android/MainActivity.cs
+++ CocktailrXamarin/CocktailrXamarin.Android/MainActivity.cs
@@ -6,20 +6,29 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Media;
 
 namespace CocktailrXamarin.Droid
 {
     [Activity(Label = "CocktailrXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+
+            await CrossMedia.Current.Initialize();
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- CocktailrXamarin/CocktailrXamarin/Views/AddSuggestionPage.xaml.cs
+++ CocktailrXamarin/CocktailrXamarin/Views/AddSuggestionPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Media;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,19 +12,46 @@ namespace CocktailrXamarin.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AddSuggestionPage : ContentPage
 	{
+        bool madePicture = false;
+
 		public AddSuggestionPage ()
 		{
 			InitializeComponent ();
         }
 
-        void Handle_Clicked(object sender, System.EventArgs e)
+        async void Handle_Clicked(object sender, System.EventArgs e)
         {
-
+            if (madePicture && cocktailName.Text != null && cocktailName.Text != "" && cocktailDescription.Text != "" && cocktailDescription.Text != null)
+            {
+                await DisplayAlert("Make a suggestion", "Thank you for your suggestion!", "Ok");
+            }
         }
 
-        void Handle_Clicked_1(object sender, System.EventArgs e)
+        async void Handle_Clicked_1Async(object sender, System.EventArgs e)
         {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
 
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                madePicture = true;
+                return stream;
+            });
         }
     }
 }
